ff5bf73c2787a95e6ebd39775499461bb3a0b5b1
Abhishek Choudhary <theabhishek.srm@gmail.com>
2023-06-23 09:44:56 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React,{useState, useEffect} from "react"; 
+
+const App = () => {
+  const [latitude, setLatitude] = useState(0)
+  const [longitude, setLongitude] = useState(0)
+  const [hemisphere, setHemisphere] = useState("")
+  const [month, setMonth] = useState(new Date().getMonth())
+
+  useEffect(() => {
+         if(navigator.geolocation){
+                navigator.geolocation.getCurrentPosition((location)=>{
+                       setLatitude(location.coords.latitude)
+                       setLongitude(location.coords.longitude)
+
+                       if(location.coords.latitude>0){
+                              setHemisphere("Northen Hemisphere")
+                       }
+                       else if(location.coords.latitude<0){
+                              setHemisphere("Southern Hemisphere")
+                       }
+                        else{
+                              setHemisphere("Equator")
+                        }
+                })
+         }
+  },[])
+
+  return(
+        <div> 
+              <h1> {latitude}, {longitude}</h1>
+              <h2> {hemisphere}</h2>
+        </div>
+  )
 }
 
 export default App;
+
+
+
+// let date = new Date(); //  date.getMonth() => 0-11
